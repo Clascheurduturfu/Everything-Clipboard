c4c397af843459e4d7683b246e210d833bf03596
Barnabé Jouanard <Jouanard.barnabe@gmail.com>
2026-05-09 21:08:01 +0200

    Update route.ts

--- website/src/app/api/checkout_sessions/route.ts
+++ website/src/app/api/checkout_sessions/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 // Initialize Stripe with the secret key from environment variables
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-02-24.acacia',
+  apiVersion: '2026-04-22.dahlia',
 });
 
 export async function POST(request: Request) {
