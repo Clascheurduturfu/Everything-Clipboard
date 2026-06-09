78d92f954153caa413ed9785c006eb6be551ef00
Barnabé Jouanard <Jouanard.barnabe@gmail.com>
2026-06-09 18:07:28 +0200

    better dmg

--- desktop-client/build_dmg.sh
+++ desktop-client/build_dmg.sh
@@ -15,13 +15,12 @@ echo "Creating DMG..."
 create-dmg \
   --volname "ClipSync" \
   --volicon "assets/clipsync.icns" \
-  --background "assets/dmg_background.png" \
   --window-pos 200 120 \
-  --window-size 800 500 \
-  --icon-size 100 \
-  --icon "ClipSync.app" 200 250 \
+  --window-size 500 300 \
+  --icon-size 128 \
+  --icon "ClipSync.app" 130 140 \
   --hide-extension "ClipSync.app" \
-  --app-drop-link 600 250 \
+  --app-drop-link 370 140 \
   "$DMG_PATH" \
   "build-output/macos/ClipSync.app"
 
